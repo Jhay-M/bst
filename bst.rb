7a5f6c43cd96b243cb8d253de43c1b3c0427c184
Joshua Malomo <rexunborn@gmail.com>
2020-11-07 17:39:04 +0100

    Started Node class

---
+++ bst.rb
@@ -0,0 +1,17 @@
+class Node
+  include Comparable
+  attr_accessor :data, :left, :right
+
+  def <=>(other)
+    data <=> other.data
+  end
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+end
+
+class Tree
+end
